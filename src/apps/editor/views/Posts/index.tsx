0669505c986077c7a298b75809999e49eee29002
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-08-26 15:46:03 +0530

    cleanup edit tags button on posts page

--- src/apps/editor/views/Posts/index.tsx
+++ src/apps/editor/views/Posts/index.tsx
@@ -27,22 +27,12 @@ export default function Posts() {
 	const unauthorizedCreate =
 		!portalProvider.permissions?.postAutoIndex && !portalProvider.permissions?.postRequestIndex;
 
-	const unauthorizedMeta = !portalProvider.permissions?.updatePortalMeta;
-
 	return (
 		<>
 			<S.Wrapper className={'fade-in'}>
 				<ViewHeader
 					header={language?.posts}
 					actions={[
-						// <Button
-						// 	type={'primary'}
-						// 	label={language?.editPostTopics}
-						// 	handlePress={() => setShowTopicAction(true)}
-						// 	disabled={unauthorizedMeta || !portalProvider.current}
-						// 	icon={ASSETS.write}
-						// 	iconLeftAlign
-						// />,
 						<Button
 							type={'alt1'}
 							label={language?.createPost}
