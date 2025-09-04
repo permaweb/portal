88bc0542164c1c4c0ef6f34560bacd0e788b092d
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-09-04 13:09:02 +0530

    cleanup unused styles

--- src/apps/editor/components/organisms/PostList/styles.ts
+++ src/apps/editor/components/organisms/PostList/styles.ts
@@ -219,26 +219,6 @@ export const PostsFooter = styled.div`
 	margin: 7.5px 0 0 0;
 `;
 
-export const PostsFooterActions = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-`;
-
-export const PostsFooterDetail = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 0 2.5px;
-
-	p {
-		color: ${(props) => props.theme.colors.font.alt1};
-		font-size: ${(props) => props.theme.typography.size.xxSmall} !important;
-		font-weight: ${(props) => props.theme.typography.weight.bold} !important;
-		font-family: ${(props) => props.theme.typography.family.primary} !important;
-	}
-`;
-
 export const WrapperEmpty = styled.div<{ type: ViewLayoutType }>`
 	width: 100%;
 	padding: 12.5px 15px;
--- src/apps/editor/components/organisms/UserList/styles.ts
+++ src/apps/editor/components/organisms/UserList/styles.ts
@@ -31,26 +31,6 @@ export const UsersFooter = styled.div`
 	margin: 7.5px 0 0 0;
 `;
 
-export const UsersFooterActions = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-`;
-
-export const UsersFooterDetail = styled.div`
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	padding: 0 2.5px;
-
-	p {
-		color: ${(props) => props.theme.colors.font.alt1};
-		font-size: ${(props) => props.theme.typography.size.xxSmall} !important;
-		font-weight: ${(props) => props.theme.typography.weight.bold} !important;
-		font-family: ${(props) => props.theme.typography.family.primary} !important;
-	}
-`;
-
 export const UsersHeaderDetailsActions = styled.div`
 	display: flex;
 	align-items: center;
