9f282b3b2ef81fddc6c7c4a06f8224fef8b7ca59
Rave | 図書館猫 <raphael.wickihalder@odysee.com>
2025-10-30 11:02:36 +0100

    Split languages, load them dynamically & add German

--- src/providers/LanguageProvider.tsx
+++ src/providers/LanguageProvider.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 
 import { loadLanguage, loadLanguageAsync, LanguageEnum, LanguageTranslations } from 'helpers/language';
 
-type LanguageKey = keyof typeof LanguageEnum;
-
 interface LanguageContextState {
 	current: LanguageKey;
 	setCurrent: (current: LanguageKey) => void;
@@ -34,47 +32,37 @@ export function LanguageProvider(props: LanguageProviderProps) {
 		return (savedLanguage as LanguageKey) || defaultLanguage;
 	});
 
-	const [translations, setTranslations] = React.useState<{ [key: string]: LanguageTranslations }>(() => {
-		const enTranslations = loadLanguage('en');
-		return {
-			en: enTranslations,
-			es: enTranslations,
-			de: enTranslations,
-		};
+	const [currentTranslations, setCurrentTranslations] = React.useState<LanguageTranslations>(() => {
+		return loadLanguage(current as string);
 	});
 
-	const [isLoading, setIsLoading] = React.useState(current !== 'en');
-
 	React.useEffect(() => {
-		const loadTranslations = async () => {
-			setIsLoading(true);
-			const enTrans = await loadLanguageAsync('en');
-			const esTrans = await loadLanguageAsync('es');
-			const deTrans = await loadLanguageAsync('de');
+		loadLanguageAsync(current as string).then((loadedTranslations) => {
+			setCurrentTranslations(loadedTranslations);
+		});
+	}, [current]);
 
-			setTranslations({
-				en: enTrans,
-				es: esTrans,
-				de: deTrans,
-			});
-			setIsLoading(false);
-		};
-
-		loadTranslations();
-	}, []);
-
-	const handleLanguageChange = React.useCallback((newLanguage: LanguageKey) => {
+	const handleLanguageChange = (newLanguage: LanguageEnum) => {
 		setCurrent(newLanguage);
 		localStorage.setItem('appLanguage', newLanguage);
 	}, []);
 
-	const contextValue = React.useMemo(
-		() => ({
-			current,
-			setCurrent: handleLanguageChange,
-			object: translations,
-		}),
-		[current, translations, handleLanguageChange]
+	const languageObject = React.useMemo(() => {
+		return {
+			[current]: currentTranslations,
+		};
+	}, [currentTranslations, current]);
+
+	return (
+		<LanguageContext.Provider
+			value={{
+				current,
+				setCurrent: handleLanguageChange,
+				object: languageObject,
+			}}
+		>
+			{props.children}
+		</LanguageContext.Provider>
 	);
 
 	return <LanguageContext.Provider value={contextValue}>{props.children}</LanguageContext.Provider>;
