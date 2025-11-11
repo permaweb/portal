2d37866b852f96ca192829dc3d462957de359f72
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-11-11 21:51:54 +0530

    home page fixes

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -19,7 +19,7 @@ export { ICONS, ICONS_SOCIAL };
 export const AO_NODE = {
 	url: 'https://hb.portalinto.com',
 	authority: 'a5ZMUKbGClAsKzB4SHDYrwkOZZHIIfpbaxrmKwUHCe8',
-	scheduler: 'NoZH3pueH0Cih6zjSNu_KRAcmg4ZJV1aGHKi0Pi5_Hc',
+	scheduler: 'a5ZMUKbGClAsKzB4SHDYrwkOZZHIIfpbaxrmKwUHCe8',
 };
 
 export const IS_TESTNET = import.meta.env.VITE_ARIO_TESTNET === 'true';
