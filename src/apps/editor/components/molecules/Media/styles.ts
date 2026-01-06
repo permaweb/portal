8d15b6d9b9a9f8eae8f6b7b3eb7c8898fe90918d
NickJ202 <nickjuliano20@gmail.com>
2026-01-06 12:26:29 -0500

    feat: remove users

--- src/apps/editor/components/molecules/Media/styles.ts
+++ src/apps/editor/components/molecules/Media/styles.ts
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 import { STYLING } from 'helpers/config';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+	width: 100%;
+`;
 
 export const Header = styled.div`
 	display: flex;
