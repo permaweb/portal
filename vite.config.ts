def3fc4c81e35e79c432c86d92ad46b339e1d77f
Bhavya Gor <bhavya.gor9999@gmail.com>
2026-01-23 17:21:33 +0530

    vite config revert

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
 
 	const config: any = {
 		editor: {
-			port: 3001,
+			port: 3000,
 			build: {
 				sourcemap: false,
 				outDir: path.resolve(__dirname, `dist/${app}`),
